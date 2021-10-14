84a72265d72e53b327d7d8c26c84059f0ad1ee6a
del <rodelcdavid@gmail.com>
2021-10-14 19:11:40 +0800

    added Picmodoro to archivelist

--- src/archiveList.js
+++ src/archiveList.js
@@ -1,129 +1,134 @@
 export const archiveList = [
-{
+  {
     id: 1,
-    title: 'Ableton Replica',
-    date: 'August 3, 2021',
-    link: 'https://rodelcdavid.github.io/archive-ui-practice/files/ableton-replica'
-},
+    title: "Ableton Replica",
+    date: "August 3, 2021",
+    link: "https://rodelcdavid.github.io/archive-ui-practice/files/ableton-replica",
+  },
 
-{
+  {
     id: 2,
-    title: 'Whatever Website',
-    date: 'August 3, 2021',
-    link: 'https://rodelcdavid.github.io/archive-ui-practice/files/whatever-website'
-},
+    title: "Whatever Website",
+    date: "August 3, 2021",
+    link: "https://rodelcdavid.github.io/archive-ui-practice/files/whatever-website",
+  },
 
-{
+  {
     id: 3,
-    title: 'navbar-1',
-    date: 'August 4, 2021',
-    link: 'https://rodelcdavid.github.io/archive-ui-practice/files/navbar-1'
-},
+    title: "navbar-1",
+    date: "August 4, 2021",
+    link: "https://rodelcdavid.github.io/archive-ui-practice/files/navbar-1",
+  },
 
-{
+  {
     id: 4,
-    title: 'navbar-2',
-    date: 'August 4, 2021',
-    link: 'https://rodelcdavid.github.io/archive-ui-practice/files/navbar-2'
-},
+    title: "navbar-2",
+    date: "August 4, 2021",
+    link: "https://rodelcdavid.github.io/archive-ui-practice/files/navbar-2",
+  },
 
-{
+  {
     id: 5,
-    title: 'Navbar Airbnb',
-    date: 'August 6, 2021',
-    link: 'https://rodelcdavid.github.io/archive-ui-practice/files/navbar-airbnb'
-},
+    title: "Navbar Airbnb",
+    date: "August 6, 2021",
+    link: "https://rodelcdavid.github.io/archive-ui-practice/files/navbar-airbnb",
+  },
 
-{
+  {
     id: 6,
-    title: 'navbar airbnb refined',
-    date: 'August 7, 2021',
-    link: 'https://rodelcdavid.github.io/archive-ui-practice/files/navbar-airbnb-refined'
-},
+    title: "navbar airbnb refined",
+    date: "August 7, 2021",
+    link: "https://rodelcdavid.github.io/archive-ui-practice/files/navbar-airbnb-refined",
+  },
 
-{
+  {
     id: 7,
-    title: 'navbar flexbox',
-    date: 'August 7, 2021',
-    link: 'https://rodelcdavid.github.io/archive-ui-practice/files/navbar-flexbox'
-},
+    title: "navbar flexbox",
+    date: "August 7, 2021",
+    link: "https://rodelcdavid.github.io/archive-ui-practice/files/navbar-flexbox",
+  },
 
-{
+  {
     id: 8,
-    title: 'navbar spacebetween',
-    date: 'August 7, 2021',
-    link: 'https://rodelcdavid.github.io/archive-ui-practice/files/navbar-spacebetween'
-},
+    title: "navbar spacebetween",
+    date: "August 7, 2021",
+    link: "https://rodelcdavid.github.io/archive-ui-practice/files/navbar-spacebetween",
+  },
 
-{
+  {
     id: 9,
-    title: 'istanbul',
-    date: 'August 9, 2021',
-    link: 'https://rodelcdavid.github.io/archive-ui-practice/files/istanbul'
-},
+    title: "istanbul",
+    date: "August 9, 2021",
+    link: "https://rodelcdavid.github.io/archive-ui-practice/files/istanbul",
+  },
 
-{
+  {
     id: 10,
-    title: 'robofriends',
-    date: 'August 18, 2021',
-    link: 'https://rodelcdavid.github.io/robofriends/'
-},
+    title: "robofriends",
+    date: "August 18, 2021",
+    link: "https://rodelcdavid.github.io/robofriends/",
+  },
 
-{
+  {
     id: 11,
-    title: 'frontend mastery',
-    date: 'August 24, 2021',
-    link: 'https://rodelcdavid.github.io/frontend-mastery/'
-},
+    title: "frontend mastery",
+    date: "August 24, 2021",
+    link: "https://rodelcdavid.github.io/frontend-mastery/",
+  },
 
-{
+  {
     id: 12,
-    title: 'Order Summary Component',
-    date: 'August 25, 2021',
-    link: 'https://rodelcdavid.github.io/archive-ui-practice/files/order-summary-component'
-},
+    title: "Order Summary Component",
+    date: "August 25, 2021",
+    link: "https://rodelcdavid.github.io/archive-ui-practice/files/order-summary-component",
+  },
 
-{
+  {
     id: 13,
-    title: 'Stats Preview',
-    date: 'August 27, 2021',
-    link: 'https://rodelcdavid.github.io/archive-ui-practice/files/stats-preview-card-component'
-},
+    title: "Stats Preview",
+    date: "August 27, 2021",
+    link: "https://rodelcdavid.github.io/archive-ui-practice/files/stats-preview-card-component",
+  },
 
-{
+  {
     id: 14,
-    title: 'React Router Fortnite',
-    date: 'August 31, 2021',
-    link: 'https://rodelcdavid.github.io/react-router-fortnite/'
-},
+    title: "React Router Fortnite",
+    date: "August 31, 2021",
+    link: "https://rodelcdavid.github.io/react-router-fortnite/",
+  },
 
-{
+  {
     id: 15,
-    title: '3 Column Preview',
-    date: 'September 3, 2021',
-    link: 'https://rodelcdavid.github.io/archive-ui-practice/files/3-column-preview'
-},
+    title: "3 Column Preview",
+    date: "September 3, 2021",
+    link: "https://rodelcdavid.github.io/archive-ui-practice/files/3-column-preview",
+  },
 
-{
+  {
     id: 16,
-    title: 'FAQ Accordion',
-    date: 'September 4, 2021',
-    link: 'https://rodelcdavid.github.io/archive-ui-practice/files/faq-accordion'
-},
+    title: "FAQ Accordion",
+    date: "September 4, 2021",
+    link: "https://rodelcdavid.github.io/archive-ui-practice/files/faq-accordion",
+  },
 
-{
+  {
     id: 17,
-    title: 'Boredom Challenge',
-    date: 'September 7, 2021',
-    link: 'https://rodelcdavid.github.io/boredom-challenge/'
-},
+    title: "Boredom Challenge",
+    date: "September 7, 2021",
+    link: "https://rodelcdavid.github.io/boredom-challenge/",
+  },
 
-{
+  {
     id: 18,
-    title: 'Article Preview',
-    date: 'September 11, 2021',
-    link: 'https://rodelcdavid.github.io/archive-ui-practice/files/article-preview'
-}
+    title: "Article Preview",
+    date: "September 11, 2021",
+    link: "https://rodelcdavid.github.io/archive-ui-practice/files/article-preview",
+  },
 
+  {
+    id: 19,
+    title: "Picmodoro",
+    date: "Oct 14, 2021",
+    link: "https://rodelcdavid.github.io/picmodoro",
+  },
 ];
-
